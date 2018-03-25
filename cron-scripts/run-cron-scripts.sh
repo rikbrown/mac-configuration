d9e0776bd8b747afc804f82d7cca333be41a3dca
Rik Brown <rik@rik.codes>
2018-03-25 00:41:19 -0700

    sync-with-git now

--- cron-scripts/run-cron-scripts.sh
+++ cron-scripts/run-cron-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ~/bin/mac-configuration/upgrade-brews.sh
-~/bin/mac-configuration/sync-brewfile.sh
+~/bin/mac-configuration/sync-with-git.sh
 
 # Go back to sleep if not in an interactive shell
 if [ "$1" == "--sleep" ]; then
