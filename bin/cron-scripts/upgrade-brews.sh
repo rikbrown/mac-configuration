8c86fafe686443039398b03cb2a716deb5063375
Rik Brown <rik@rik.codes>
2018-03-31 14:48:29 -0700

    Force upgrade some apps

--- bin/cron-scripts/upgrade-brews.sh
+++ bin/cron-scripts/upgrade-brews.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
+
 sudo -S -v <<< 'SECRETPASSWORD'
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 brew upgrade
 brew cask upgrade
+
+# Some apps are marked as auto-update, but are tedious to autoupdate (IntelliJ makes you download a dmg, the horror!)
+brew cask upgrade intellij-idea iterm2 google-chrome
+
 brew cleanup -s
 brew cask cleanup
